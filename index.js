a77275d0c25f080fe88f4c3eb9c7a1041fc55c1b
Dennis <dennischen123@gmail.com>
2020-08-03 21:51:10 -0700

    test

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function handleMessage(sender_psid, received_message) {
         console.log(translatedResponse);
         response = {
             // "text": translatedResponse
-            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+            "text": `You sent the message: "${translatedResponse}". Now send me an image!`
         }
     }
 
