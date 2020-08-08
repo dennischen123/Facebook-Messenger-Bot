cca37e51436daaf35010b6e21d1b32621c3e35cd
Dennis <dennischen123@gmail.com>
2020-08-07 18:41:32 -0700

    testing setup postback

--- index.js
+++ index.js
@@ -18,12 +18,70 @@ app.get('/', ((req, res) => {
     );
 }))
 
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
+        // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
+    }
+});
+
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {
+    // Parse the request body from the POST
+    let body = req.body;
+    // Check the webhook event is from a Page subscription
+    if (body.object === 'page') {
+        // Iterate over each entry - there may be multiple if batched
+        body.entry.forEach(function(entry) {
+            // Gets the body of the webhook event
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
+        });
+        // Return a '200 OK' response to all events
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        // Return a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+    }
+});
+
+
+
+
 // Handles messages events
 async function handleMessage(sender_psid, received_message) {
     try {
         let response;
+        let setupStatus = false;
         // Check if the message contains text
-        if (received_message.text) {
+        if (received_message.text.toLowerCase() == 'setup') {
+            response = handleSetup(response);
+        } else if (received_message.text && setupStatus) {
             // Create the payload for a basic text message
 
             // translate(input, fromLangCode, toLangCode,)
@@ -31,7 +89,6 @@ async function handleMessage(sender_psid, received_message) {
             let translated = await translatedResponse.json();
             console.log(translated.outputs[0].output);
             response = {
-                // "text": translatedResponse
                 "text": `Translation: "${translated.outputs[0].output}" .`
             }
         }
@@ -40,12 +97,55 @@ async function handleMessage(sender_psid, received_message) {
         console.log(error)
     }
 
+}
 
+function handleSetup(response) {
+    response = {
+        "payload": {
+            "template_type": "generic",
+            "elements": [{
+                "title": "Select Your Language",
+                "subtitle": "Tap a button to answer.",
+                // "image_url": attachment_url,
+                "buttons": [{
+                        "type": "postback",
+                        "title": "English",
+                        "payload": "en",
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Chinese",
+                        "payload": "zh",
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Spanish",
+                        "payload": "es",
+                    }
+                ],
+            }]
+        }
+    }
+    return response;
 }
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
+    let response;
 
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
+    if (payload === 'en') {
+        response = { "text": "Translating from English" }
+    } else if (payload === 'zh') {
+        response = { "text": "Translating from Chinese" }
+    } else if (payload === 'es') {
+        response = { "text": "Translating from Spanish" }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
 }
 
 // Sends response messages via the Send API
@@ -74,77 +174,6 @@ function callSendAPI(sender_psid, response) {
 }
 
 
-// Creates the endpoint for our webhook 
-app.post('/webhook', (req, res) => {
-
-    // Parse the request body from the POST
-    let body = req.body;
-
-    // Check the webhook event is from a Page subscription
-    if (body.object === 'page') {
-
-        // Iterate over each entry - there may be multiple if batched
-        body.entry.forEach(function(entry) {
-
-            // Gets the body of the webhook event
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-
-
-            // Get the sender PSID
-            let sender_psid = webhook_event.sender.id;
-            console.log('Sender PSID: ' + sender_psid);
-
-            // Check if the event is a message or postback and
-            // pass the event to the appropriate handler function
-            if (webhook_event.message) {
-                handleMessage(sender_psid, webhook_event.message);
-            } else if (webhook_event.postback) {
-                handlePostback(sender_psid, webhook_event.postback);
-            }
-
-        });
-
-        // Return a '200 OK' response to all events
-        res.status(200).send('EVENT_RECEIVED');
-
-    } else {
-        // Return a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
-    }
-
-});
-
-// Adds support for GET requests to our webhook
-app.get('/webhook', (req, res) => {
-
-    // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
-
-    // Parse the query params
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
-
-        // Checks the mode and token sent is correct
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-            // Responds with the challenge token from the request
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
-
-        } else {
-            // Responds with '403 Forbidden' if verify tokens do not match
-            res.sendStatus(403);
-        }
-    }
-});
-
-
-
 
 // Sets server port and logs message on success
-app.listen(PORT, () => console.log(`webhook is listenin on port ${PORT}`));
+app.listen(PORT, () => console.log(`webhook is listening on port ${PORT}`));
